06bb83311cacbdb8a9b09a2c0caec88b50c68e9d
MuelitasVill <mmartinezve@unal.edu.co>
2025-12-10 07:47:45 -0500

    add: lab etical haking

--- app/test/test_database_connection.py
+++ app/test/test_database_connection.py
@@ -1,12 +1,13 @@
 from app.configuration.database import engine
 import pytest
+from sqlalchemy import text
 from sqlalchemy.exc import OperationalError
 
 
 def test_database_connection():
     try:
         with engine.connect() as connection:
-            result = connection.execute("SELECT 1")
+            result = connection.execute(text("SELECT 1"))
             assert result.scalar() == 1
     except OperationalError as e:
         pytest.fail(f"Could not connect to the database: {e}")
